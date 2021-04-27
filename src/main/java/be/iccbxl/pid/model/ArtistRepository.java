51e4f3871bc2c26857e9b2c47d15f4c803d572eb
bennour Aymen <bennour.ma@gmail.com>
2021-04-28 00:40:34 +0200

    Modeles + Migration Tables Locality et type

--- src/main/java/be/iccbxl/pid/model/ArtistRepository.java
+++ src/main/java/be/iccbxl/pid/model/ArtistRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface ArtistRepository extends CrudRepository<Artist, Long> {
 	List<Artist> findByLastname(String lastname);
-
+	
 	Artist findById(long id);
 }
---
+++ src/main/java/be/iccbxl/pid/model/Type.java
@@ -0,0 +1,36 @@
+package be.iccbxl.pid.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="types")
+public class Type {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+    private String type;
+	
+	protected Type() {}
+
+	public Type(String type) {
+		this.type = type;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+    @Override
+    public String toString(){
+        return "Type [id=" + id + ", type=" + type + "]";
+    }
+	
+}
